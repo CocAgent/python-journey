d68d3d894de1abd1bf035d5ec68a9838dd4c6012
Tran Thao Suong <thaosuongtran660@gmail.com>
2026-05-18 18:42:55 +0700

    Update ex01_hello.py
    đã hoàn thành bài tập và thêm vào ở dòng 45 -> 69

--- weeks/week-01-hello-python/exercises/ex01_hello.py
+++ weeks/week-01-hello-python/exercises/ex01_hello.py
@@ -42,4 +42,30 @@ Chạy file này bằng: python ex01_hello.py
 # Viết code ở đây:
 
 
+"""================================
+Bài tập 01: Hello Python! 🐍
+# TODO 1: In ra dòng chữ "Hello, World!"
+print("Hello, World!")
+
+# TODO 2: In ra tên của bạn trên một dòng riêng
+print("Trần Thảo Sương")
+
+# TODO 3: In ra 3 điều bạn thích, mỗi điều trên một dòng
+print("Tôi thích ăn bánh mì")
+print("Tôi thích nghe nhạc")
+print("Tôi thích code Python")
+
+# TODO 4: In ra một hình chữ nhật bằng dấu *
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
+
+# TODO 5 (Thử thách): In ra tên bạn bằng ký tự đặc biệt
+print("TTTTTTTTT  SSSSSS")
+print("    T      S")
+print("    T      SSSSSS")
+print("    T           S")
+print("    T      SSSSSS")
+
 print("\n🎉 Chúc mừng! Bạn đã hoàn thành bài tập đầu tiên!")
