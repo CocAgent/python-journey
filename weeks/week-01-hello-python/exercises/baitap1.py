6b39af40c75759fefc5460204c97277103ecbd14
Hopsadboi <hoahop7a1@gmail.com>
2026-05-18 19:10:34 +0700

    Add baitap1.py to exercises directory
    sửa file tuan 1

---
+++ weeks/week-01-hello-python/exercises/baitap1.py
@@ -0,0 +1,34 @@
+"""
+Bài tập 01: Hello Python! 🐍
+================================
+Mục tiêu: Làm quen với hàm print()
+
+Hãy hoàn thành các TODO bên dưới.
+Chạy file này bằng: python ex01_hello.py
+"""
+
+print("Hello, World!")
+
+# TODO 2
+print("Nguyễn Văn Minh")
+
+# TODO 3
+print("Tôi thích ăn phở")
+print("Tôi thích nghe nhạc")
+print("Tôi thích code Python")
+
+# TODO 4
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
+
+# TODO 5 (Ví dụ: HI)
+print("H   H  III")
+print("H   H   I")
+print("HHHHH   I")
+print("H   H   I")
+print("H   H  III")
+
+print("\n🎉 Chúc mừng! Bạn đã hoàn thành bài tập đầu tiên!")
+
