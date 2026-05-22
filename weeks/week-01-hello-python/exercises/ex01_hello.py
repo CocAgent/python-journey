425e7109878c76de23b9780b32c0dfcd8dc069a9
tranphuong12345 <ducphuong24032020@gmail.com>
2026-05-22 16:11:16 +0700

    Implement hello world and personal introduction
    Hoàn thành ex01.py _Phương

--- weeks/week-01-hello-python/exercises/ex01_hello.py
+++ weeks/week-01-hello-python/exercises/ex01_hello.py
@@ -9,10 +9,12 @@ Chạy file này bằng: python ex01_hello.py
 
 # TODO 1: In ra dòng chữ "Hello, World!"
 # Viết code ở đây:
+print("Hello, World!")
 
 
 # TODO 2: In ra tên của bạn trên một dòng riêng
 # Viết code ở đây:
+print("Trần Đức Phương")
 
 
 # TODO 3: In ra 3 điều bạn thích, mỗi điều trên một dòng
@@ -21,7 +23,9 @@ Chạy file này bằng: python ex01_hello.py
 # Tôi thích nghe nhạc
 # Tôi thích code Python
 # Viết code ở đây:
-
+print("Tôi thích nghe nhạc")
+print("Tôi thích xem phim")
+print("Tôi thích chạy bộ")
 
 # TODO 4: In ra một hình chữ nhật bằng dấu *
 # Kết quả mong muốn:
@@ -30,7 +34,10 @@ Chạy file này bằng: python ex01_hello.py
 # *   *
 # *****
 # Viết code ở đây:
-
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
 
 # TODO 5 (Thử thách): In ra tên bạn bằng ký tự đặc biệt
 # Ví dụ nếu tên là "HI":
@@ -40,6 +47,10 @@ Chạy file này bằng: python ex01_hello.py
 # H   H   I
 # H   H  III
 # Viết code ở đây:
-
+print("PPPPP  H   H  U   U  OOO   N   N  GGGG")
+print("P   P  H   H  U   U O   O  NN  N  G")
+print("PPPP   HHHHH  U   U O   O  N N N  G  GG")
+print("P      H   H  U   U O   O  N  NN  G   G")
+print("P      H   H   UUU   OOO   N   N   GGGG")
 
 print("\n🎉 Chúc mừng! Bạn đã hoàn thành bài tập đầu tiên!")
