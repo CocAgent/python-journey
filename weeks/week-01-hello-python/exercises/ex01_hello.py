3448bfc006ed04df3bdef44ad1e702a00bdb4a26
Tran Thao Suong <thaosuongtran660@gmail.com>
2026-05-22 22:09:57 +0700

    hoan thanh week1

--- weeks/week-01-hello-python/exercises/ex01_hello.py
+++ weeks/week-01-hello-python/exercises/ex01_hello.py
@@ -9,7 +9,7 @@ Chạy file này bằng: python ex01_hello.py
 
 # TODO 1: In ra dòng chữ "Hello, World!"
 # Viết code ở đây:
-
+print("Hello, World!")
 
 # TODO 2: In ra tên của bạn trên một dòng riêng
 # Viết code ở đây:
@@ -30,7 +30,10 @@ Chạy file này bằng: python ex01_hello.py
 # *   *
 # *****
 # Viết code ở đây:
-
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
 
 # TODO 5 (Thử thách): In ra tên bạn bằng ký tự đặc biệt
 # Ví dụ nếu tên là "HI":
