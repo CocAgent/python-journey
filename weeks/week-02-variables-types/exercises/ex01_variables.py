e1fd5f47755dc4bd2ec9308be69b3b7f5a61adec
Nguyen Tuan Kiet <Nguyentuankiet210666@gmail.com>
2026-09-09 13:49:44 +0700

    Hoan thanh bai tap ex01 variables

--- weeks/week-02-variables-types/exercises/ex01_variables.py
+++ weeks/week-02-variables-types/exercises/ex01_variables.py
@@ -48,3 +48,7 @@ print("Sau bước 4: x =", x)
 # TODO 4 (Thử thách): Multiple assignment
 # Gán 3 biến trên 1 dòng: ho, ten, tuoi = ???
 # In ra: "Họ tên: [ho] [ten], [tuoi] tuổi"
+ho = "Nguyễn"
+ten = "A"
+tuoi = 25
+print(f"Họ tên: {ho} {ten}, {tuoi} tuổi")
