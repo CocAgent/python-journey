291480cb8fab0f36eea7b2808c9a25a0c8c9da66
taizzz23 <bingay546@gmail.com>
2026-09-12 14:40:39 +0700

    Complete mini-project week 01

--- weeks/week-01-hello-python/mini-project/starter.py
+++ weeks/week-01-hello-python/mini-project/starter.py
@@ -11,14 +11,18 @@ ten = input("Nhập tên của bạn: ")
 
 # Bước 2: Tính độ rộng khung
 # TODO: Tính width dựa trên len(ten)
-
+width = len(ten) + 10
 # Bước 3: In khung trên
 # TODO: In dòng trên bằng ╔═══╗
-
+print("╔" + "═" * width + "╗")
 # Bước 4: In nội dung
 # TODO: In tên trong khung, căn giữa
+print("║" + "Xin chào".center(width) + "║")
+print("║" + ten.upper().center(width) + "║")       # ten.upper() để viết hoa tên cho nổi bật
+print("║" + "🐍 Python 🐍".center(width) + "║")
 
 # Bước 5: In khung dưới
 # TODO: In dòng dưới bằng ╚═══╝
+print("╚" + "═" * width + "╝")
 
 # Gợi ý: Dùng str.center(width) để căn giữa
