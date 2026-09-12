753be508fbb1981b85e14c6578701dc0ba76e68f
Nguyen Tan Dung <dungnguyen2az@gmail.com>
2026-09-12 16:04:31 +0700

    NTD666 BT week 01

--- weeks/week-01-hello-python/exercises/ex01_hello.py
+++ weeks/week-01-hello-python/exercises/ex01_hello.py
@@ -9,11 +9,19 @@ Chạy file này bằng: python ex01_hello.py
 
 # TODO 1: In ra dòng chữ "Hello, World!"
 # Viết code ở đây:
+<<<<<<< HEAD
+print("Hello, World! ")
+
+# TODO 2: In ra tên của bạn trên một dòng riêng
+# Viết code ở đây:
+print("Tan Dung")
+=======
 
 
 # TODO 2: In ra tên của bạn trên một dòng riêng
 # Viết code ở đây:
 
+>>>>>>> 52d01c78eff5db54d540c0f453ea18f216d9ba5c
 
 # TODO 3: In ra 3 điều bạn thích, mỗi điều trên một dòng
 # Ví dụ:
@@ -22,6 +30,13 @@ Chạy file này bằng: python ex01_hello.py
 # Tôi thích code Python
 # Viết code ở đây:
 
+<<<<<<< HEAD
+print("Tôi thích ăn phở")
+print("Tôi thích nghe nhạc")
+print("Tôi thích code Python")
+
+=======
+>>>>>>> 52d01c78eff5db54d540c0f453ea18f216d9ba5c
 
 # TODO 4: In ra một hình chữ nhật bằng dấu *
 # Kết quả mong muốn:
@@ -31,6 +46,14 @@ Chạy file này bằng: python ex01_hello.py
 # *****
 # Viết code ở đây:
 
+<<<<<<< HEAD
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
+
+=======
+>>>>>>> 52d01c78eff5db54d540c0f453ea18f216d9ba5c
 
 # TODO 5 (Thử thách): In ra tên bạn bằng ký tự đặc biệt
 # Ví dụ nếu tên là "HI":
@@ -41,5 +64,12 @@ Chạy file này bằng: python ex01_hello.py
 # H   H  III
 # Viết code ở đây:
 
+<<<<<<< HEAD
+print("DDDD   U   U  NN   N  GGGG ")
+print("D   D  U   U  N N  N  G    ")
+print("D   D  U   U  N  N N  G  GG")
+print("DDDD    UUU   N   NN  GGGG ")
+=======
+>>>>>>> 52d01c78eff5db54d540c0f453ea18f216d9ba5c
 
 print("\n🎉 Chúc mừng! Bạn đã hoàn thành bài tập đầu tiên!")
